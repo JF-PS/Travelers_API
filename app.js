60010fc7dbbe159d3c3e1c269be118c767cdf9fe
Jean-François Picherit-Steinbrucker <79050225+JF-Picherit@users.noreply.github.com>
2021-11-14 17:46:51 +0100

    test

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var path = require('path');
-var app = express();
-var port = 8000;
-var http = require('http');
-var cors = require('cors');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const cors = require('cors');
 require('colors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+
+io.on('connect', (socket) => {
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(" ================================================ ")
+    console.log(" USER IS DISCONNECT ")
+  });
+
+  socket.on('sendLocation', ({ id, lat, lng }) => {
+    io.emit('location', { id, lat, lng });
+  });
+
+});
 
 const UsersRepository = require('./repositories/users.pg');
 const AnnoncesRepository = require('./repositories/annonces.pg.js');
@@ -24,15 +44,9 @@ const usersRepository = new UsersRepository();
 const geolocalisationsRepository = new GeolocalisationsRepository();
 const annoncesRepository = new AnnoncesRepository();
 
-app.use(express.static('public'));
-
-const server = http.createServer(app).listen(port, () => {
-  var desc = "Adresse du serveur: ";
-  var adresse = ` http://localhost:${port}`.green.bold;
-  console.log(`################################################################`.yellow.bold);
-  console.log(desc + adresse);
-  console.log(`################################################################`.yellow.bold);
-});
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const path = require('path');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -40,12 +54,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
 
 app.use('/users', userRoutes(express, usersController(usersRepository)));
 app.use('/geolocalisations', geolocalisationRoutes(express, geolocalisationsController(geolocalisationsRepository)));
 app.use('/annonces', annonceRoutes(express, annoncesController(annoncesRepository)));
 
-
-
- module.exports = app;
+server.listen(port, () => {
+  var desc = "Adresse du serveur: ";
+  var adresse = ` http://localhost:${port}`.green.bold;
+  console.log(`################################################################`.yellow.bold);
+  console.log(desc + adresse);
+  console.log(`################################################################`.yellow.bold);
+});
+module.exports = app;
